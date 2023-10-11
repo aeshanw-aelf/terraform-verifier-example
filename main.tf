73ee9dbf7e0603fe43d45909f9c15354bf6d1395
Aeshan Wijetunge <aeshan.w@aelf.io>
2023-10-11 10:55:08 +0800

    modify TF with did-server settings

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
+# Define an absolute path for the volume mount
+locals {
+  logs_volume_host_path = pathexpand("./did-verifier-silo/Logs")
+}
+
 provider "docker" {}
 provider "time" {}
 
@@ -52,35 +57,97 @@ resource "docker_container" "mongodb" {
 #   }
 # }
 
-resource "time_sleep" "wait_50_seconds" {
+resource "time_sleep" "mongodb_wait_50_seconds" {
   depends_on = [docker_container.mongodb]
 
   create_duration = "50s"
 }
 
-resource "docker_container" "did-verifier-silo" {
-  image = "portkeydid/did-server-silo:mainnet-latest"
-  name  = "did-verifier-silo"
-
-  depends_on = [time_sleep.wait_50_seconds]
-
-  # ports {
-  #   internal = 80
-  #   external = 8002 //debug what ports are used by DID-verifier
-  # }
+# Define a Docker network
+resource "docker_network" "verifier" {
+  name = "verifier"
 }
 
-resource "docker_container" "did-verifier-api" {
-  image = "portkeydid/did-server-api:mainnet-latest"
-  name  = "did-verifier-api"
+# Define a Docker container for did-verifier-silo
+resource "docker_container" "did_verifier_silo" {
+  name         = "did-verifier-silo"
+  image        = "portkeydid/did-verifier-silo:mainnet-latest"
+  restart      = "always"
 
-  depends_on = [docker_container.did-verifier-silo]
+  depends_on = [time_sleep.mongodb_wait_50_seconds]
+
+  network_mode = docker_network.verifier.name
 
   ports {
-    internal = 4200
-    external = 4200 //debug what ports are used by DID-verifier
+    internal = 9010
+    external = 9010
   }
+
+  ports {
+    internal = 10010
+    external = 10010
+  }
+
+  ports {
+    internal = 20010
+    external = 20010
+  }
+
+  volumes {
+    container_path = "/etc/localtime"
+    host_path      = "/etc/localtime"
+    read_only      = true
+  }
+
+  # TODO need to fix these paths
+  volumes {
+    container_path = "/app/appsettings.json"
+    host_path      = pathexpand("~/did-verifier-silo/appsettings.json")
+  }
+
+  volumes {
+    container_path = "/app/Logs"
+    host_path      = local.logs_volume_host_path
+  }
+
+  privileged = true
 }
+
+# Define a Docker container for did-verifier-api
+resource "docker_container" "did_verifier_api" {
+  name         = "did-verifier-api"
+  image        = "portkeydid/did-verifier-api:mainnet-latest"
+  restart      = "always"
+
+  network_mode = docker_network.verifier.name
+
+  ports {
+    internal = 8010
+    external = 8010
+  }
+
+  volumes {
+    container_path = "/etc/localtime"
+    host_path      = "/etc/localtime"
+    read_only      = true
+  }
+
+  # TODO need to fix these paths
+  volumes {
+    container_path = "/app/appsettings.json"
+    host_path      = "./did-verifier-api/appsettings.json"
+  }
+
+  volumes {
+    container_path = "/app/Logs"
+    host_path      = "./did-verifier-api/Logs"
+  }
+
+  depends_on = [docker_container.did_verifier_silo]
+
+  privileged = true
+}
+
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
